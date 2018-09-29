5873e74d85cf444c070e385f8cbe14107119ed0e
iVin <ivinvirus@gmail.com>
2018-09-29 20:25:35 +1000

    - Implemented validate.js - Added client side validation to create and edit function - Added a date picker - Added date formatting across the whole page - Removed ng-disabled button usage, switched over to validate.js instead - Added jquery function to reset error messages

--- WebApplication4/WebApplication4/Angular/ApplicationController.js
+++ WebApplication4/WebApplication4/Angular/ApplicationController.js
@@ -1,4 +1,221 @@
-﻿angular.module("myApplication", ["smart-table"]).controller('safeCtrl', ['$scope', '$http', function ($scope, $http) {
+﻿
+
+angular.module("myApplication", ["smart-table", "angularValidator"]).controller('safeCtrl', ['$scope', '$http', function ($scope, $http) {
+
+
+    (function () {
+        // Before using it we must add the parse and format functions
+        // Here is a sample implementation using moment.js
+        validate.extend(validate.validators.datetime, {
+            // The value is guaranteed not to be null or undefined but otherwise it
+            // could be anything.
+            parse: function (value, options) {
+                return +moment.utc(value);
+            },
+            // Input is a unix timestamp
+            format: function (value, options) {
+                var format = options.dateOnly ? "YYYY-MM-DD" : "YYYY-MM-DD hh:mm:ss";
+                return moment.utc(value).format(format);
+            }
+        });
+
+        // These are the constraints used to validate the form
+        var constraints = {
+
+            name: {
+                presence: { allowEmpty: false },
+                format: {
+                    // We don't allow anything that a-z and 0-9
+                    pattern: "[a-zA-Z]+",
+                    // but we don't care if the username is uppercase or lowercase
+                    message: "must be alphabetic."
+                },
+                length: {
+                    maximum: 20,
+                }
+            },
+
+            sex: {
+                presence: { allowEmpty: false, message: "^Please select the gender." }
+            },
+
+            dob: {
+                presence: { allowEmpty: false, message: "^Please select the date." }
+            },
+
+            address: {
+                presence: { allowEmpty: false },
+                length: {
+                    maximum: 50
+                }
+            },
+
+            income: {
+                presence: { allowEmpty: false },
+                numericality: {
+                    onlyInteger: true,
+                    greaterThanOrEqualTo: 0
+                }
+            }
+        };
+
+
+
+        // Hook up the form so we can prevent it from being posted
+        var form = document.querySelector("form#myForm");
+        form.addEventListener("submit", function (ev) {
+            ev.preventDefault();
+            handleFormSubmit(form);
+        });
+
+        var form3 = document.querySelector("form#myForm3");
+        form3.addEventListener("submit", function (ev) {
+            ev.preventDefault();
+            handleFormSubmit(form);
+        });
+
+
+
+        // Hook up the inputs to validate on the fly
+        var inputs = document.querySelectorAll("input, textarea, select")
+        for (var i = 0; i < inputs.length; ++i) {
+            inputs.item(i).addEventListener("change", function (ev) {
+                var errors = validate(form, constraints) || {};
+                showErrorsForInput(this, errors[this.name])
+            });
+        }
+
+        for (var i = 0; i < inputs.length; ++i) {
+            inputs.item(i).addEventListener("change", function (ev) {
+                var errors2 = validate(form3, constraints) || {};
+                showErrorsForInput(this, errors2[this.name])
+            });
+        }
+
+
+
+        function handleFormSubmit(form, input) {
+            // validate the form aainst the constraints
+            var errors = validate(form, constraints);
+            // then we update the form to reflect the results
+            showErrors(form, errors || {});
+            if (!errors) {
+                //showSuccess();
+            }
+        }
+
+        // Updates the inputs with the validation errors
+        function showErrors(form, errors) {
+            // We loop through all the inputs and show the errors for that input
+            _.each(form.querySelectorAll("input[name], select[name]"), function (input) {
+                // Since the errors can be null if no errors were found we need to handle
+                // that
+                showErrorsForInput(input, errors && errors[input.name]);
+            });
+        }
+
+        // Shows the errors for a specific input
+        function showErrorsForInput(input, errors) {
+            // This is the root of the input
+            var formGroup = closestParent(input.parentNode, "form-group")
+                // Find where the error messages will be insert into
+                , messages = formGroup.querySelector(".messages");
+            // First we remove any old messages and resets the classes
+            resetFormGroup(formGroup);
+            // If we have errors
+            if (errors) {
+                // we first mark the group has having errors
+                formGroup.classList.add("has-error");
+                // then we append all the errors
+                _.each(errors, function (error) {
+                    addError(messages, error);
+                });
+            } else {
+                // otherwise we simply mark it as success
+                formGroup.classList.add("has-success");
+            }
+        }
+
+        // Recusively finds the closest parent that has the specified class
+        function closestParent(child, className) {
+            if (!child || child == document) {
+                return null;
+            }
+            if (child.classList.contains(className)) {
+                return child;
+            } else {
+                return closestParent(child.parentNode, className);
+            }
+        }
+
+        function resetFormGroup(formGroup) {
+            // Remove the success and error classes
+            formGroup.classList.remove("has-error");
+            formGroup.classList.remove("has-success");
+            // and remove any old messages
+            _.each(formGroup.querySelectorAll(".help-block.error"), function (el) {
+                el.parentNode.removeChild(el);
+            });
+        }
+
+        // Adds the specified error with the following markup
+        // <p class="help-block error">[message]</p>
+        function addError(messages, error) {
+            var block = document.createElement("p");
+            block.classList.add("help-block");
+            block.classList.add("error");
+            block.innerText = error;
+            messages.appendChild(block);
+        }
+
+        function showSuccess() {
+            // We made it \:D/
+            alert("Success!");
+        }
+    }
+
+
+    )();
+
+
+    var validationConstraint = {
+
+        Name: {
+            presence: { allowEmpty: false },
+            format: {
+                // We don't allow anything that a-z and 0-9
+                pattern: "[a-zA-Z]+",
+                // but we don't care if the username is uppercase or lowercase
+                message: "must be alphabetic."
+            },
+            length: {
+                maximum: 20,
+            }
+        },
+
+        Sex: {
+            presence: { allowEmpty: false , message: "^Please select the gender." }
+        },
+
+        DOB: {
+            presence: { allowEmpty: false, message: "^Please select the date." }
+        },
+
+        Address: {
+            presence: { allowEmpty: false },
+            length: {
+                maximum: 50
+            }
+        },
+
+        Money: {
+            presence: { allowEmpty: false },
+            numericality: {
+                onlyInteger: true,
+                greaterThanOrEqualTo: 0
+            }
+        }
+    };
 
     var iController = this;
 
@@ -60,43 +277,62 @@
 
         var person = { Name: $scope.name, Sex: $scope.gender, DOB: $scope.dob, Address: $scope.address, Money: $scope.income };
 
+        console.log(person);
+        console.log(validate(person, validationConstraint));
 
-        var saveSubs = saveAPI(person);
-        saveSubs.then(function (d) {
-            $('#modal1').modal('hide');
-            $http.get("http://localhost:49330/api/PeopleAPI/LoadData").then(function (response) {
-                $scope.rowCollection = response.data;
-            });
-            document.getElementById("myForm").reset();
-            console.log("Successfully inserted data!");
+        //name, sex, dob, address, income
 
-        }, function (error) {
-            console.log('Oops! Something went wrong while saving the data.');
-        })
+        if (!validate(person, validationConstraint)) {
+            var saveSubs = saveAPI(person);
+            saveSubs.then(function (d) {
+
+                $('#modal1').modal('hide');
+                $http.get("http://localhost:49330/api/PeopleAPI/LoadData").then(function (response) {
+                    $scope.rowCollection = response.data;
+                });
+                document.getElementById("myForm").reset();
+                person = {};
+                console.log("Successfully inserted data!");
+
+            }, function (error) {
+                console.log('Oops! Something went wrong while saving the data.');
+                })
+
+        }
+        
+
+
+        
     };
 
     //Edit Data Function
     $scope.editData = function () {
-
+        
         var person = $scope.formObject;
-        person.DOB = "2/2/2";
 
-        var editFunction = iController.editAPI2(person);
-
-        editFunction.then(function (d) {
-
-            $('#modal3').modal('hide');
-
-            iController.getData();
-
-            //document.getElementById("myForm3").reset();
-            console.log("Successfully inserted data!");
+        console.log(person);
+        console.log(validate(person, validationConstraint));
 
 
+        if (!validate(person, validationConstraint)) {
+            var editFunction = iController.editAPI2(person);
 
-        }, function (error) {
-            console.log('Oops! Something went wrong while saving the data.');
-        })
+            editFunction.then(function (d) {
+
+                $('#modal3').modal('hide');
+
+                iController.getData();
+
+                //document.getElementById("myForm3").reset();
+                console.log("Successfully inserted dataa!");
+
+
+
+            }, function (error) {
+                console.log('Oops! Something went wrong while saving the data.');
+            })
+        }
+        
     };
 
     //Details Method, Gets data by ID
@@ -107,9 +343,14 @@
 
     $scope.getDataID2 = function (ID) {
         $scope.formObject = $scope.rowCollection.filter(x => x.PersonID == ID)[0];
+        $scope.formObject.DOB = new Date($scope.formObject.DOB);
         $('#modal3').modal('show');
     }
 
+    $scope.openCreate = function () {
+        $(".messages").empty();
+        $('#modal1').modal('show');
+    }
 
 
     //Delete Data Function
@@ -123,4 +364,23 @@
     };
 
     //--------------------- [/Functions]---------------------------------------
+
+
+    //----------------[Client Side Validations]-------------------------
+
+    //Name
+    $scope.nameCheck = function (name) {
+        if (name != "Test") {
+            return "It works";
+        }
+    }
+
+
+    //Gender
+    //Age
+    //DOB
+    //Income
+    //Address
+
+    //----------------[/Client Side Validations]------------------------
 }]);
